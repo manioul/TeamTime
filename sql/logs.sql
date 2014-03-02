362ce1cc1e5304aad325edda0bb93f31259551bb
manioul <webmaster@teamtime.me>
2014-03-02 23:22:34 +0100

    Création d'une procédure permettant de loguer des messages

--- sql/logs.sql
+++ sql/logs.sql
@@ -4,11 +4,12 @@ CREATE TABLE TBL_MESSAGES_SYSTEME (
 	utilisateur VARCHAR(63) NOT NULL,
 	catégorie set('DEBUG','INFO','ERREUR','LOG','USER') NOT NULL DEFAULT 'USER',
 	-- La catégorie USER est destinée à afficher un message à l'utilisateur
-	appelant VARCHAR(64) NOT NULL,
+	appelant VARCHAR(64) NOT NULL DEFAULT 'unknown',
 	short tinytext NOT NULL,
 	message text NOT NULL,
 	contexte text NOT NULL,
 	timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	lu BOOLEAN DEFAULT FALSE,
 	PRIMARY KEY (mid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -17,7 +18,7 @@ DROP PROCEDURE IF EXISTS messageSystem|
 CREATE PROCEDURE messageSystem( IN msg TEXT , IN cat VARCHAR(64) , IN app VARCHAR(64) , IN court TINYTEXT , IN context TEXT )
 BEGIN
 	IF cat IS NULL THEN
-		SET cat = 'DEBUG';
+		SET cat = 'USER';
 	END IF;
 	IF app IS NULL THEN
 		SET app = 'unknown';
@@ -32,7 +33,10 @@ BEGIN
 	INSERT INTO TBL_MESSAGES_SYSTEME
 	(mid, utilisateur, catégorie, appelant, short, message, contexte)
 	VALUES
-	(NULL, CURRENT_USER(), cat, app, court, msg, context); 
+	(NULL, USER(), cat, app, court, msg, context); 
+	-- FIXME http://dev.mysql.com/worklog/task/?id=4647
+	-- CURRENT_USER() renvoie toujours le créateur de la routine
+	-- CURRENT_USER(USER()) retourne une erreur...
 END
 |
 
