d709e845bb076df6e20e7733911e75c268b0053c
manioul <webmaster@teamtime.me>
2014-03-14 01:11:25 +0100

    Mise à jour numéro de version - v1.5d

--- constantes.inc.php
+++ constantes.inc.php
@@ -26,7 +26,7 @@
 define('INSTALL_DIR', "/var/www/TeamTime");
 
 // Numéro de version de l'application
-define('VERSION', "1.5c");
+define('VERSION', "1.5d");
 
 // Les constantes relatives à des erreurs critiques sont préfixées par ERR_
 // Les constantes relatives à des erreurs fatales sont préfixées par FAT_
