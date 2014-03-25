bccc1c0134fd64c963ca1097c143beb3efd0b1f3
manioul <webmaster@teamtime.me>
2014-03-26 00:15:53 +0100

    Mise à jour numéro de version - v2.0a
    Des changements importants ont été apportés dans le fonctionnement de
    TeamTime, justifiant le changement de numéro de version majeur :
    - Les occupations (stages, congés...) des utilisateurs sont directement
      gérées par la base de données. Cela améliore notamment la consistance
    au niveau du décompte de congés. La période de recouvrement (01/01 au
    31/03) est à peu près prise en charge. Seuls de rares cas ne sont pas
    gérés (si un utilisateur a épuisé ses congés de l'année passée avant la
    date limite de dépôt et a déjà déposé des congés de l'année suivante
    avant la date limite de dépôt de l'année passée, si il annule des congés
    de l'année passé, les congés de l'année suivante ne seront pas ramenés
    automatiquement sur le décompte de l'année passée.)
    - Les titres d'annulation de congés sont enfin gérés. Lorsqu'un
      utilisateur annule des congés dont les titres ont déjà été édités, un
    titre d'annulation est enregistré. Il suffit d'aller éditer le titre
    d'annulation via un lien disponible sur la page de gestion des congés.
    - Amélioration du support multi-équipes/multi-centres. Un utilisateur
      qui change d'affectation peut suivre son planning (basculement
    automagique vers la nouvelle affectation). Si l'utilisateur change
    d'affectation en cours de cycle, il sera automagiquement grisé et
    considéré absent à la date de changement. Les dates de changements sont
    gérées dans le profil de l'utilisateur.
    - Plusieurs corrections de bug

--- constantes.inc.php
+++ constantes.inc.php
@@ -26,7 +26,7 @@
 define('INSTALL_DIR', "/var/www/TeamTime");
 
 // Numéro de version de l'application
-define('VERSION', "1.5d");
+define('VERSION', "2.0a");
 
 // Les constantes relatives à des erreurs critiques sont préfixées par ERR_
 // Les constantes relatives à des erreurs fatales sont préfixées par FAT_
