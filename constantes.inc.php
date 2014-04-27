79b5aaa3ef276f90a672d754298ef4e101d3d89f
manioul <webmaster@teamtime.me>
2014-04-27 16:43:22 +0200

    Mise à jour numéro de version - v2.1a
    Cette version ajoute la possibilité de créer et gérer des activités pour
    son équipe.
    Un formulaire permet de créer les activités pour son équipe.
    Un tableau offre la possibilité de supprimer, (dés)activer des activités
    et de les ordonner dans le menu déroulant.
    Les demi-équipes sont gérés : une activité peut compter pour une demi
    présence, il faut donc deux utilisateurs pour une présence.
    L'affichage des compteurs est possible sur l'affichage grilles
    multiples.
    Le compteur de présence inclus, en première position, le décompte de
    présents moins les utilisateurs qui ont une activité de type dispo.
    Les utilisateurs sont triés par ordre alphabétique dans la plupart des
    listes (hors liste équipe qui requiert l'ordre au sein de l'équipe).
    Corrections de bugs.
    Corrections de style.

--- constantes.inc.php
+++ constantes.inc.php
@@ -26,7 +26,7 @@
 define('INSTALL_DIR', "/var/www/TeamTime");
 
 // Numéro de version de l'application
-define('VERSION', "2.0d");
+define('VERSION', "2.1a");
 
 // Les constantes relatives à des erreurs critiques sont préfixées par ERR_
 // Les constantes relatives à des erreurs fatales sont préfixées par FAT_
