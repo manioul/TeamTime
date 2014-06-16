871b398cb0ef87e7b12d6e64f14b9745517887ed
manioul <webmaster@teamtime.me>
2014-06-16 11:47:19 +0200

    Correction typo

--- sql/heures.sql
+++ sql/heures.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `TBL_HEURES_A_PARTAGER` (
 	  `dispatched` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Positionné lorsque les heures ont été calculées',
 	  `writable` tinyint(1) NOT NULL DEFAULT '1',
 	  PRIMARY KEY (`centre`,`team`,`date`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Le nombre d''heures à paratager par jour';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Le nombre d''heures à partager par jour';
 
 CREATE TABLE IF NOT EXISTS `TBL_DISPATCH_HEURES` (
 	  `rid` int(11) NOT NULL AUTO_INCREMENT,
