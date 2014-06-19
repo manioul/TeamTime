580acb07c79803b87e32405303054c9bfc7fc16f
manioul <webmaster@teamtime.me>
2014-06-19 14:01:00 +0200

    Mise à jour numéro de version - v2.2a
    Suppression du filtre pour l'affichage des heures des utilisateurs. Ce
    filtre n'avait plus son utilité.
    
    Affichage des compteurs plus clair (le nom long, le nom d'une activité
    renvoyant au compteur, étaient utilisés en lieu et place du nom du
    compteur). Le nom du compteur est, maintenant, affiché sur le décompte
    des évènements spéciaux et dans les compteurs sur la grille.
    /!\ Pensez à utiliser un nom parlant pour vos compteurs.
    
    Suppression de TBL_CLASSE. Les classes des utilisateurs sont gérées de
    manière dynamique en fonction de leur affectation.
    
    Optimisation du code (méthode getActiveUsersCell allégée).
    
    Mise à jour du thème : le pied de page est placé en bas de page,
    utilisation de blocs plus cohérents (classe w24 => 240px), intégration
    de messages.css dans general.css...
    
    Possibilité pour les Editeurs de modifier les infos de leurs
    utilisateurs.
    
    Possiblité de s'enregistrer. Le support des nouveaux utilisateurs est
    encore partiel.
    
    update.sql permet une mise à jour de la base de données à partir d'une
    version vers les versions supérieures.
    
    La structure de la base de données est mise à jour lors des commit.
    
    Correction de nombreux bugs (calcul des présents dans le cas d'un
    changement d'équipe apparaîssant sur la grille, utilisation de
    array_key_exists, tag non fermé dans le code html, calcul des heures...)

--- constantes.inc.php
+++ constantes.inc.php
@@ -26,7 +26,7 @@
 define('INSTALL_DIR', "/var/www/TeamTime");
 
 // Numéro de version de l'application
-define('VERSION', "2.1c");
+define('VERSION', "2.2a");
 
 // Les constantes relatives à des erreurs critiques sont préfixées par ERR_
 // Les constantes relatives à des erreurs fatales sont préfixées par FAT_
