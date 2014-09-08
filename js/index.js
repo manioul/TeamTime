da2b79d2e2dd9b9d5e303227fa9619f2adf8e0f5
manioul <webmaster@teamtime.me>
2014-09-08 16:21:13 +0200

    Les entrées de support sur la page index s'affiche au clic et plus au survol

--- js/index.js
+++ js/index.js
@@ -10,6 +10,12 @@ $(function() {
 		$('#fConn').css('display', 'none');
 		$('#fConn').removeClass('largeConnexion');
 		});
+	$('#nav3-text h1').toggle(function() {
+		$('#dSupport ul').css('display', 'block');
+		}
+		,function() {
+		$('#dSupport ul').css('display', 'none');
+		});
 	$('#nav4-text h1').toggle(function() {
 		$('#fSignup').css('display', 'block');
 		}
