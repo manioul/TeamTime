36de06e4b92ffe7149275862e2ea13fcc8f6dac2
manioul <webmaster@teamtime.me>
2014-11-15 00:43:01 +0100

    Mise à jour du numéro de version - v2.4a
    Ajout d'une page de recherche et de création d'utilisateur accessible
    aux Editeurs. Celle-ci permet d'intégrer un utilisateur qui existe déjà
    dans la base (ou pas) dans son équipe.
    Les membres de teamEdit ont maintenant accès à l'édition des comptes des
    utilisateurs de leur équipe.
    Ajoute des messages de log lors de la connexion.
    Corrections de quelques bugs.

--- constantes.inc.php
+++ constantes.inc.php
@@ -26,7 +26,7 @@
 define('INSTALL_DIR', "/var/www/TeamTime");
 
 // Numéro de version de l'application
-define('VERSION', "2.3e");
+define('VERSION', "2.4a");
 
 // Les constantes relatives à des erreurs critiques sont préfixées par ERR_
 // Les constantes relatives à des erreurs fatales sont préfixées par FAT_
